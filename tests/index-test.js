d27647fd96fa8cf951a808fa2f832f427d46ceb5
Inhyuk Park <exilee20c@users.noreply.github.com>
2018-12-05 18:12:08 +0900

    export css files and testcase

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,23 +1,29 @@
-import expect from 'expect'
-import React from 'react'
-import {render, unmountComponentAtNode} from 'react-dom'
+import expect from "expect";
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
 
-import Component from 'src/'
+import Component from "src/";
 
-describe('Component', () => {
-  let node
+describe("Component", () => {
+  let node;
 
   beforeEach(() => {
-    node = document.createElement('div')
-  })
+    node = document.createElement("div");
+  });
 
   afterEach(() => {
-    unmountComponentAtNode(node)
-  })
+    unmountComponentAtNode(node);
+  });
 
-  it('displays a welcome message', () => {
-    render(<Component/>, node, () => {
-      expect(node.innerHTML).toContain('Welcome to React components')
-    })
-  })
-})
+  it("props seccess", () => {
+    render(
+      <Component w={100} h={100} x={10} y={10} z={10} mw={50} mh={50} draggable={true} onFinish={() => {}} color="red">
+        <div style={{ width: "100%", height: "100%", backgroundColor: "#cccccc" }} />
+      </Component>,
+      node,
+      () => {
+        expect(node.innerHTML).toContain('style="width: 100px; height: 100px; left: 10px; top: 10px; z-index: 100;"');
+      }
+    );
+  });
+});
